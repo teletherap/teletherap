af307878778badc14a253b4e9cc6e286023fc3a3
sepehr ilami <sepehrilami@yahoo.com>
2022-08-09 01:57:54 +0430

    Add review model

--- backend/therapy/models.py
+++ backend/therapy/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Reservation(models.Model):
@@ -15,3 +16,14 @@ class Reservation(models.Model):
 
     communication_type = models.CharField(max_length=5, choices=CommunicationType.choices,
                                           default=CommunicationType.TELEGRAM)
+                                        
+class Review(models.Model):
+    reservation = models.OneToOneField('therapy.Reservation', on_delete=models.CASCADE,
+                                       related_name='review')
+    rating = models.IntegerField(null=True,
+                                 validators=[
+                                    MinValueValidator(1),
+                                    MaxValueValidator(5)])
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
