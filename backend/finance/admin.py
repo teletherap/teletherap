29d7c5c959dda3ad3f508f97e1ca7fb3d2907215
sepehr ilami <sepehrilami@yahoo.com>
2022-08-09 01:41:06 +0430

    Add models admins

--- backend/finance/admin.py
+++ backend/finance/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Wallet, Deposit, Withdrawal, ReservationTransaction
+
+
 # Register your models here.
+admin.site.register(Wallet)
+admin.site.register(Deposit)
+admin.site.register(Withdrawal)
+admin.site.register(ReservationTransaction)
