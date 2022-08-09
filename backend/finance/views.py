10a0fa3afadd59e3b3ca54db008d69ec5ee68aa6
Kimia Noorbakhsh <kimianoorbakhsh@gmail.com>
2022-08-09 19:33:08 +0430

    implementation of wallet withdrawal (#6)
    * implementation of wallet withdrawal
    
    Co-authored-by: Alireza Ilami <sepehrilami@users.noreply.github.com>
    
    * Fix: view and url
    
    Co-authored-by: Alireza Ilami <sepehrilami@users.noreply.github.com>
    Co-authored-by: ahmadsalimi <ahsa9978@gmail.com>

--- backend/finance/views.py
+++ backend/finance/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import permissions, views, status, response
+from django.db import transaction
+from django.http import HttpRequest
+
+from .models import Withdrawal
+
 
 # Create your views here.
+class Withdraw(views.APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    @transaction.atomic
+    def post(self, request: HttpRequest, *args, **kwargs):
+        amount = float(request.data.get('amount'))
+        iban = request.data.get('iban')
+        
+        if amount is None:
+            return response.Response({'error': 'amount is required'}, status=status.HTTP_400_BAD_REQUEST)
+        if iban is None:
+            return response.Response({'error': 'iban is required'}, status=status.HTTP_400_BAD_REQUEST)
+        if amount > request.user.wallet.balance:
+            return response.Response({'error': 'insufficient balance'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        request.user.wallet.balance -= amount
+        request.user.wallet.save()
+        Withdrawal.objects.create(user=request.user, amount=amount, destination_iban=iban)
+        return response.Response({'success': 'withdrawal successful'}, status=status.HTTP_200_OK)
