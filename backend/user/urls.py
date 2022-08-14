4427eca1f496f62c267cfa1747a6f2e45e7a5f25
Ahmad Salimi <44668836+ahmadsalimi@users.noreply.github.com>
2022-08-14 17:14:06 +0430

    Feature: User info API (#12)

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 
 urlpatterns = [
+    re_path(r'^$', views.UserInfoView.as_view(), name='user_info'),
     re_path(r'^login/$', views.MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     re_path(r'^login/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
     re_path(r'^register/client/$', views.ClientRegisterView.as_view(), name='register_client'),
