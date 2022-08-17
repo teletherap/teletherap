95829eda780e9a1d6628bda8579c07536064810e
ahmadsalimi <ahsa9978@gmail.com>
2022-08-17 20:04:35 +0430

    Fix: Reservation and cancelation verifications

--- backend/therapy/views.py
+++ backend/therapy/views.py
@@ -1,7 +1,9 @@
+from django.db import transaction
 from django.http import HttpRequest
 from rest_framework import viewsets, permissions, exceptions
 
 from user import models as user_models, serializers as user_serializers, permissions as user_permissions
+from finance import models as finance_models
 from . import models, serializers
 
 
@@ -13,13 +15,31 @@ class PublicTherapistRL(viewsets.ReadOnlyModelViewSet):
 
 class BaseReservationViewSet(viewsets.ModelViewSet):
     queryset = models.Reservation.objects.all()
-    serializer_class = serializers.ClientReservationSerializer
+    serializer_class = serializers.ReservationSerializer
 
-    def perform_destroy(self, instance: models.Reservation):
-        if instance.state == models.Reservation.State.ATTENDED:
+    @transaction.atomic
+    def perform_destroy(self, reservation: models.Reservation):
+        if reservation.state == models.Reservation.State.ATTENDED:
             raise exceptions.PermissionDenied('You cannot delete an attended reservation.')
-        instance.state = models.Reservation.State.CANCELLED
-        instance.save()
+
+        if reservation.state == models.Reservation.State.CANCELLED:
+            return
+
+        reservation_transaction: finance_models.ReservationTransaction = \
+            finance_models.ReservationTransaction.objects.get(reservation=reservation)
+        reservation_transaction.is_rollbacked = True
+        reservation_transaction.save()
+
+        reservation.state = models.Reservation.State.CANCELLED
+        reservation.save()
+
+        therapist_wallet: finance_models.Wallet = finance_models.Wallet.objects.get(user=reservation.therapist.user)
+        therapist_wallet.balance -= reservation_transaction.amount
+        therapist_wallet.save()
+
+        client_wallet: finance_models.Wallet = finance_models.Wallet.objects.get(user=reservation.client.user)
+        client_wallet.balance += reservation_transaction.amount
+        client_wallet.save()
 
 
 class ClientReservation(BaseReservationViewSet):
